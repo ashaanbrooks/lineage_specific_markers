7bd2c9b68acd279a76e8c69aad76724768540d18
Ash Brooks <ashaanbrooks@gmail.com>
2026-05-31 21:20:52 -0700

    Untrack files that should be removed

--- scripts/decompress.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#SBATCH --time=00:10:00
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
-
-time tar -xf "$1"
--- scripts/get_assemblies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while IFS=$'\t' read -r name address; do
-    wget -O "$name" "$address"
-done < names_addresses.tsv
